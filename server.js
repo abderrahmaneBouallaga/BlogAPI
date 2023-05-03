8daaed64e0b25682cb34f53e84c1dc0317ba38f6
abderrahmaneBouallaga <106495994+abderrahmaneBouallaga@users.noreply.github.com>
2023-05-03 12:36:21 +0100

    Create server.js

---
+++ server.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+
+dotenv.config({ path: './config.env' })
+const app = require('./app')
+
+// CONECTION TO DATA BASE //
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD)
+
+mongoose.connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+}).then(() => {
+    console.log('DB Connection successful 🟢')
+}).catch((err) => {
+    console.log(err)
+})
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`app running on port ${port} 🟢`)
+})
