b7455d765f0b53b3d1c66731d6679bcbc03e2700
abderrahmaneBouallaga <106495994+abderrahmaneBouallaga@users.noreply.github.com>
2023-05-03 12:36:36 +0100

    Create postRoutes.js

---
+++ routes/postRoutes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const postsController = require('./../controllers/postsController')
+
+const router = express.Router()
+
+router.route('/').get(postsController.getAllPosts).post(postsController.createPost)
+router.route('/:id').get(postsController.getPost).patch(postsController.updatePost).delete(postsController.deletePost)
+
+module.exports = router;
