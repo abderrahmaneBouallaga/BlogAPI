bb0b47eda6c43905c2a677d66006519895eaca4e
abderrahmaneBouallaga <106495994+abderrahmaneBouallaga@users.noreply.github.com>
2023-05-03 23:19:47 +0100

    Update postsController.js

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -1,99 +1,99 @@
-const Post = require('./../models/postsModel')
-const APIFeatures = require('./../utils/apiFeatures')
+const Post = require("./../models/postsModel");
+const APIFeatures = require("./../utils/apiFeatures");
 
 exports.getAllPosts = async (req, res) => {
-    try {
+  try {
+    // EXECUTE QUERY //
+    const features = new APIFeatures(Post.find(), req.query)
+      .filter()
+      .sort()
+      .limit()
+    const post = await features.query;
 
-        // EXECUTE QUERY //
-        const features = new APIFeatures(Post.find(), req.query).filter()
-        const post = await features.query
-        console.log()
-
-        res.status(200).json({
-            status: 'success',
-            result: post.length,
-            data: {
-                post
-            }
-        })
-    }catch(err) {
-        res.status(404).json({
-            status: 'fail',
-            message: err
-        })
-    }
-}
+    res.status(200).json({
+      status: "success",
+      result: post.length,
+      data: {
+        post,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: "fail",
+      message: err,
+    });
+    console.log(err);
+  }
+};
 
 exports.getPost = async (req, res) => {
-    try {
-        const post = await Post.findById(req.params.id);
+  try {
+    const post = await Post.findById(req.params.id);
 
-        res.status(200).json({
-            status: 'success',
-            data: {
-                post
-            }
-        })
-        
-    }catch(err) {
-        res.status(404).json({
-            status: 'fail',
-            message: err
-        })
-    }
-}
+    res.status(200).json({
+      status: "success",
+      data: {
+        post,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: "fail",
+      message: err,
+    });
+  }
+};
 
 exports.createPost = async (req, res) => {
-    try {
-        const newPost = await Post.create(req.body)
+  try {
+    const newPost = await Post.create(req.body);
 
-        res.status(201).json({
-            status: 'success',
-            data: {
-                post: newPost
-            }
-        })
-    }catch(err) {
-        res.status(400).json({
-            status: 'fail',
-            message: err
-        })
-    }
-}
+    res.status(201).json({
+      status: "success",
+      data: {
+        post: newPost,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "fail",
+      message: err,
+    });
+  }
+};
 
 exports.updatePost = async (req, res) => {
-    try{
-        const post = await Post.findByIdAndUpdate(req.params.id, req.body, {
-            new: true,
-            runValidators: true
-        })
-        
-        res.status(200).json({
-            status: 'success',
-            data: {
-                post
-            }
-        })
-    }catch(err){
-        res.status(404).json({
-            status: 'fail',
-            message: err
-        })
-        
-    }
-}
+  try {
+    const post = await Post.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    res.status(200).json({
+      status: "success",
+      data: {
+        post,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: "fail",
+      message: err,
+    });
+  }
+};
 
 exports.deletePost = async (req, res) => {
-    try {
-        const post = await Post.findByIdAndDelete(req.params.id)
-        res.status(204).json({
-            status: 'success',
-            data: null
-        })
-    }catch(err) {
-        res.status(404).json({
-            status: 'fail',
-            message: err
-        })
-    }
-}
+  try {
+    const post = await Post.findByIdAndDelete(req.params.id);
+    res.status(204).json({
+      status: "success",
+      data: null,
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: "fail",
+      message: err,
+    });
+  }
+};
