c5d296c62600970d223a15f82e71577c5f28ad07
abderrahmaneBouallaga <106495994+abderrahmaneBouallaga@users.noreply.github.com>
2023-05-04 14:21:14 +0100

    Update postsModel.js

--- models/postsModel.js
+++ models/postsModel.js
@@ -20,8 +20,38 @@ const postSchema = new mongoose.Schema({
     },
     updatedAt: {
         type: Date
+    },
+    secretPost: {
+        type: Boolean,
+        default: false
     }
 })
+
+
+//DOCUMENT MIDDLEWARE: uns before .save() and .create()
+
+postSchema.pre('save', function(next) {
+    this.title = this.title.charAt(0).toUpperCase() + this.title.slice(1).toLowerCase()
+    const authorArr = this.author.toLowerCase().split(' ')
+    this.author = authorArr.map(part => part.charAt(0).toUpperCase() + part.slice(1)).join(' ')
+    next()
+})
+
+// QUERY MIDDLEWARE
+
+postSchema.pre(/^find/, function(next){   /// /^find/ for find - findOne
+    this.find({ secretPost: { $ne: true }})
+    next();
+})
+
+
+// AGGREGATION MIDDLEWARE
+
+postSchema.pre('aggregate', function(next) {
+    this.pipeline().unshift({ $match: { secretPost: { $ne: true } }})
+    next()
+})
+
 
 const Post = mongoose.model('Post', postSchema)
 
