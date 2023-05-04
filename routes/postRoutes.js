6c3590797506be8d0240ebc0c36fa89b91be040a
abderrahmaneBouallaga <106495994+abderrahmaneBouallaga@users.noreply.github.com>
2023-05-04 14:21:16 +0100

    Update postRoutes.js

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -3,6 +3,9 @@ const postsController = require('./../controllers/postsController')
 
 const router = express.Router()
 
+
+router.route('/the-posts-of-today').get(postsController.getNewPosts)
+
 router.route('/').get(postsController.getAllPosts).post(postsController.createPost)
 router.route('/:id').get(postsController.getPost).patch(postsController.updatePost).delete(postsController.deletePost)
 
