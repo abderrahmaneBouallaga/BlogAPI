324f765d86762a5431daf5716a8ae18dd9f57caa
abderrahmaneBouallaga <106495994+abderrahmaneBouallaga@users.noreply.github.com>
2023-05-12 19:42:12 +0100

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const authController = require('../controllers/authController')
-
+const userController = require('../controllers/userController')
 const router = express.Router();
 
 router.post('/signup', authController.signup)
@@ -8,5 +8,10 @@ router.post('/login', authController.login)
 
 router.post('/forgotPassword', authController.forgotPassword)
 router.patch('/resetPassword/:token', authController.resetPassword)
+
+router.patch('/updateMyPassword', authController.protect, authController.updatePassword);
+
+router.patch('/updateMe', authController.protect, userController.updateMe)
+router.delete('/deleteMe', authController.protect, userController.deleteMe);
 
 module.exports = router;
