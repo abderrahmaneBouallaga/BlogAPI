ec24fedb36850bd3d702accc146f0123337f500d
abderrahmaneBouallaga <106495994+abderrahmaneBouallaga@users.noreply.github.com>
2023-05-12 19:42:09 +0100

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,4 @@
+const AppError = require('../utils/appError');
 const User = require('./../models/usersModel');
 const catchAsync = require('./../utils/catchAsync')
 
@@ -11,3 +12,32 @@ exports.getAllUsers = catchAsync(async (req, res) => {
     })
 })
 
+exports.updateMe = catchAsync(async (req, res, next) => {
+    //1. Create error if user POSTs password data
+    if(req.body.password || req.body.passwordConfirm) {
+        return next(new AppError('This route is not for password updates. Please user /updateMyPassword.', 400))
+    }
+    //2. Filtered out unwanted fields name that are not allowed to be updated
+    const filteredBody = filterObj(req.body, 'name', 'email');
+    //3. Update user document
+    const updatedUser = await User.findByIdAndDelete(req.user.id, filteredBody, {
+        new: true,
+        runValidators: true
+    });
+
+    res.status(200).json({
+        status: 'success',
+        data: {
+            user: updatedUser
+        }
+    })
+})
+
+exports.deleteMe = catchAsync(async (req, res, next) => {
+    await User.findByIdAndUpdate(req.user.id, {active: false});
+
+    res.statusI(204).json({
+        status: 'success',
+        data: null
+    })
+})
